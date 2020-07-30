f66db494ba8b724eea052c823ebc0a746536fde2
kirbyhayes <hayeskirby@gmail.com>
2020-07-30 14:09:56 -0700

    Project and .R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Taking the inverse of a matrix can be computationally expensive
+## thus you may want to cache the inverse of a matrix rather than compute 
+## that particular inverse multiple times.
 
-## Write a short comment describing this function
+## makeCacheMatrix is defining the "get" and set" condition in the environment so an
+## inverse is taken if one does not exist in the cached environment.
 
 makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+                set <- function(y) {
+                        x <<- y
+                        inv <<- NULL
+                }
+                #defining the "get" and "set" conditions in the environment
+                get <- function() x
+                setInverse <- function(inverse) inv <<- inverse
+                getInverse <- function() inv
+                list(set = set,
+                     get = get,
+                     setInverse = setInverse,
+                     getInverse = getInverse)
+        }
+        
 
-}
+## cacheSolve looks for a cached inverse for 'x' before it calculates the inverse
+## if a cached inverse exists cacheSolve will return the cached inverse value 
+## while displaying the message 'cached inverse matrix found, getting the matrix'.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)  {
+        inv <- x$getInverse()
+        ## the "!" in front of is.null checks that inv is "not NULL" and the 'if' returns:
+        ## 'cached inverse matrix found, getting the matrix' when inv is not NULL
+        if (!is.null(inv)) {
+                message("cached inverse matrix found, getting the matrix")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
